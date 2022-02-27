6bb705219665ecbaa645c188f3d68eb1f1d5efa9
sulao1999 <59861460+sulao1999@users.noreply.github.com>
2022-02-26 18:14:48 -1000

    landing

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
@@ -8,10 +9,28 @@ class Footer extends React.Component {
       <footer>
         <div style={divStyle} className="ui center aligned container">
           <hr />
+          <Grid columns={2} divided>
+            <Grid.Row>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
               Department of Information and Computer Sciences <br />
               University of Hawaii<br />
               Honolulu, HI 96822 <br />
-          <a href="http://ics-software-engineering.github.io/meteor-application-template-react">Template Home Page</a>
+                <a href="https://github.com/bug-catchers">Home Page</a>
+              </Grid.Column>
+              <Grid.Column>
+                Developer<br/>
+                <a href="https://chakhon.github.io/">Chak Hon Lam</a>
+                <br/>
+                <a href="https://ianbm.github.io/">Ian Manzano</a>
+                <br/>
+                <a href="https://shengt-jin.github.io/">Shengtong Jin</a>
+                <br/>
+                <a href="https://sulao1999.github.io/">Su Lao</a>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </div>
       </footer>
     );
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,24 +1,56 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Header, Icon } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
+import { withRouter } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
-
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
-
-      </Grid>
+      <div className="landing-background">
+        <Grid columns={2} id='landing-page' verticalAlign='middle' textAlign='center' container>
+          <Grid.Row>
+            <Header as='h1'>Welcome! User</Header>
+          </Grid.Row>
+          { this.props.currentUser ? (
+            <Grid.Row>
+              <Grid.Column>
+                <Header as='h2'>Budget Planners</Header>
+                <Icon name='edit' size='huge'/>
+                <Header as='h3'>View and track your budget.</Header>
+              </Grid.Column>
+              <Grid.Column>
+                <Header as='h2'>Create Planner</Header>
+                <Icon name='list alternate outline' size='huge'/>
+                <Header as='h3'>Create a new budget planner.</Header>
+              </Grid.Column>
+            </Grid.Row>) :
+            (
+              <div>
+                <br/>
+                <Header as='h2'> Sign up now to get started with Budget Planner! </Header>
+                <Header as='h2'> Sign in to view your Budget. </Header>
+                <br/>
+              </div>
+            )
+          }
+        </Grid>
+      </div>
     );
   }
 }
 
-export default Landing;
+// Declare the types of all properties.
+Landing.propTypes = {
+  currentUser: PropTypes.string,
+};
+
+// withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+const LandingContainer = withTracker(() => ({
+  currentUser: Meteor.user() ? Meteor.user().username : '',
+}))(Landing);
+
+// Enable ReactRouter for this component. https://reacttraining.com/react-router/web/api/withRouter
+export default withRouter(LandingContainer);
