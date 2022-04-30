5fd007ffb8deed9daa74219e90b6f286338a9c71
ShengT-Jin <66652454+ShengT-Jin@users.noreply.github.com>
2022-04-30 13:12:12 -1000

    fix
    fix

--- app/imports/ui/pages/Feedback.jsx
+++ app/imports/ui/pages/Feedback.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Grid, Segment, Header } from 'semantic-ui-react';
-import { AutoForm, ErrorsField, LongTextField, SubmitField, TextField } from 'uniforms-semantic';
+import { AutoForm, ErrorsField, LongTextField, SelectField, SubmitField, TextField } from 'uniforms-semantic';
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
@@ -9,6 +9,7 @@ import SimpleSchema from 'simpl-schema';
 const formSchema = new SimpleSchema({
   title: String,
   feedback: String,
+  option: String,
 });
 
 const bridge = new SimpleSchema2Bridge(formSchema);
@@ -18,13 +19,14 @@ class Feedback extends React.Component {
 
   // On submit, insert the data.
   submit(data, formRef) {
-    const { title, feedback } = data;
+    const { title, feedback, option } = data;
     const owner = Meteor.user().username;
 
     Meteor.call(
       'sendEmail',
       (`${title} from ${owner}`),
       (`${feedback}`),
+      (`${option}`),
     );
     formRef.reset();
   }
@@ -39,6 +41,7 @@ class Feedback extends React.Component {
           <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)} >
             <Segment>
               <TextField name='title'/>
+              <SelectField name='option' showInlineError={true} placeholder={'Select one'} options={[{ label: 'Delete My Account', value: 'Delete My Account' }, { label: 'Do Not Delete My Account', value: 'Do Not Delete My Account' }]}/>
               <LongTextField name='feedback'/>
               <SubmitField value='Submit'/>
               <ErrorsField/>
